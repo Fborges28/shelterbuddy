6ba38ff7d206f1d0cc2c2e9679ce2cb9ecfdd8e9
Felipe Borges <felipe.borges@meiuca.design>
2022-06-10 01:35:22 -0300

    <tests> API tests list created

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders shelter buddy logo', () => {
-  render(<App />);
-  const logoElement = screen.getByAltText(/Shelter Buddy logo/i);
-  expect(logoElement).toBeInTheDocument();
+describe('Check the APP UI data', () => {
+  it('renders shelter buddy logo', () => {
+    render(<App />);
+    const logoElement = screen.getByAltText(/Shelter Buddy logo/i);
+    expect(logoElement).toBeInTheDocument();
+  });
 });
+
+
---
+++ src/services/ShelterService.test.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+describe('Requests from the ShelterBuddy Animal API', () => {
+  it.todo("retrieves the AnimalList endpoint data");
+  it.todo("retrieves the AnimalPhotoList endpoint data");
+  it.todo("find an animal by Name");
+  it.todo("find an animal photo by Id");
+  it.todo("get the animal total list count");
+});
