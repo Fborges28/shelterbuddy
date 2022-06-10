8fcaf9724017bb7f6b10895c59f9655a3c16c957
Felipe Borges <felipe.borges@meiuca.design>
2022-06-09 21:40:10 -0300

    <feature> Header component created

---
+++ src/shared/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent as Logo } from "@/shared/assets/logo_shelterbuddy.svg";
+import "./styles.scss";
+
+function Header() {
+  return (
+    <header>
+        <Logo />
+    </header>
+  )
+}
+
+export default Header;
