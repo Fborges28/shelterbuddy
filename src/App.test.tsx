a3df08405eee995075333a41a8046c852db517fa
Felipe Borges <felipe.borges@meiuca.design>
2022-06-10 00:59:14 -0300

    <fix> Fixing test name (logo)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders shelter buddy logo', () => {
   render(<App />);
   const logoElement = screen.getByAltText(/Shelter Buddy logo/i);
   expect(logoElement).toBeInTheDocument();
