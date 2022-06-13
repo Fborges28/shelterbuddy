7369306ab3bdeeac541886b13a4bcb897c0efec6
Felipe Borges <felipe.borges@meiuca.design>
2022-06-13 00:07:12 -0300

    <feature> ShelterCard created

---
+++ src/shared/components/ShelterCard/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Stack, Typography, Accordion, AccordionSummary, AccordionDetails, AccordionActions, Button } from '@mui/material';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Animal } from '@/shared/components/ShelterAnimalTable/TableBody';
+import pet from "@/shared/assets/pet.png";
+
+
+function ShelterAnimalCard({name, breed , color , gender , type }: Animal) {
+  return (
+    <Accordion sx={{ boxShadow: "none", backgroundColor: "var(--light-gray-color)", marginBottom: "12px", border: "none" }} >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+            <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+                <img className="thumbnail-photo" src={pet} alt="Animal"/>
+                <Typography>{name || "-"}</Typography>
+            </Stack>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>Type: {type}</Typography>
+            <Typography>Breed: {breed}</Typography>
+            <Typography>Gender: {gender}</Typography>
+            <Typography>Color: {color}</Typography>
+        </AccordionDetails>
+        <AccordionActions>
+            <Button 
+                size="large" 
+                variant="contained" 
+                endIcon={<ChevronRightIcon />} 
+                fullWidth 
+                onClick={() => alert('details')}>
+                Details
+            </Button>
+        </AccordionActions>
+    </Accordion>
+  )
+}
+
+export default ShelterAnimalCard
